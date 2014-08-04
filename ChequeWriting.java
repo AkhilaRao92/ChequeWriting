0e55ff04aeef80aa4eb7f6401b5bc24e5f3e0a61
ebayuser <ebayuser@paypal.com>
2014-08-04 16:14:22 +0530

    western format edit by team 4

--- ChequeWriting.java
+++ ChequeWriting.java
@@ -1,14 +1,14 @@
 import java.util.*;
 public class ChequeWriting{
-
+    
     static String[] oneToNineteen = { " ", "ONE ", "TWO ", "THREE ", "FOUR ", "FIVE ", "SIX ",
-            "SEVEN ", "EIGHT ", "NINE ", "TEN ", "ELEVEN ", "TWELVE ",
-            "THIRTEEN ", "FOURTEEN ", "FIFTEEN ", "SIXTEEN ", "SEVENTEEN ",
-            "EIGHTEEN ", "NINETEEN " };
+        "SEVEN ", "EIGHT ", "NINE ", "TEN ", "ELEVEN ", "TWELVE ",
+        "THIRTEEN ", "FOURTEEN ", "FIFTEEN ", "SIXTEEN ", "SEVENTEEN ",
+        "EIGHTEEN ", "NINETEEN " };
     
     static String[] tens = { " ", " ", "TWENTY ", "THIRTY ", "FOURTY ", "FIFTY ",
-            "SIXTY ", "SEVENTY ", "EIGHTY ", "NINETY " };
-            
+        "SIXTY ", "SEVENTY ", "EIGHTY ", "NINETY " };
+    
     static HashMap<Integer,String> numToWordMap = new HashMap<Integer,String>(){
         {
             put(1,"");
@@ -23,50 +23,52 @@ public class ChequeWriting{
     static  List<String> denoms = Arrays.asList("HUNDRED","THOUSAND","LAKH","CRORE");
     static List<Integer> powersOfTen = Arrays.asList(10000000,100000,1000,100,10,1);
     
+    
+    
     public static String getPaise(String amount)
     {
-        String paise = String.valueOf(amount); 
+        String paise = String.valueOf(amount);
         paise = paise.split("\\.")[1];
-        return paise; 
+        return paise;
     }
-     public static String getRupee(String amount)
+    public static String getRupee(String amount)
     {
-        String rupee = String.valueOf(amount); 
+        String rupee = String.valueOf(amount);
         rupee = rupee.split("\\.")[0];
-        return rupee; 
+        return rupee;
     }
-
-	public static String updatePaiseFormat(String paise)
-	{
-		if (paise.length() <2)
-			paise += "0";
-		if (paise.length()>2)
-			paise = paise.substring(0, Math.min(paise.length(), 2));
-		return paise;
-	}
-
-	public static String paiseWords(String paise)
-	{
-		String[] tens = {"", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninty"};
-		String[] ones = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-		String paiseWord=new String();
-
-		char[] paiseChar = paise.toCharArray();
-		int count = 1;
-		for(char p: paiseChar)
-		{
-			if (count==1)
-				paiseWord +=tens[p-'0'];
-			else
-				paiseWord +=ones[p-'0'];
-			paiseWord +=" ";
-			count++;
-		}
-
-		return paiseWord;
-	}
-
-            
+    
+    public static String updatePaiseFormat(String paise)
+    {
+        if (paise.length() <2)
+            paise += "0";
+        if (paise.length()>2)
+            paise = paise.substring(0, Math.min(paise.length(), 2));
+        return paise;
+    }
+    
+    public static String paiseWords(String paise)
+    {
+        String[] tens = {"", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninty"};
+        String[] ones = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        String paiseWord=new String();
+        
+        char[] paiseChar = paise.toCharArray();
+        int count = 1;
+        for(char p: paiseChar)
+        {
+            if (count==1)
+                paiseWord +=tens[p-'0'];
+            else
+                paiseWord +=ones[p-'0'];
+            paiseWord +=" ";
+            count++;
+        }
+        
+        return paiseWord;
+    }
+    
+    
     public static String numToStr(int amount) {
         String str = "";
         int highestPower = 0;
@@ -74,9 +76,9 @@ public class ChequeWriting{
         
         if(amount == 0)
             return new String("");
-            
-        for(int num : powersOfTen){
         
+        for(int num : powersOfTen){
+            
             if(amount/num > 0){
                 highestPower = num;
                 break;
@@ -85,10 +87,10 @@ public class ChequeWriting{
         
         if (amount >= highestPower) {
             if(highestPower == 10){
-                if(amount >=20)
-                str += tens[ amount / highestPower] + oneToNineteen[ amount % highestPower]  ;
+                if(amount >= 20)
+                    str += tens[ amount / highestPower] + oneToNineteen[ amount % highestPower]  ;
                 else
-                str += oneToNineteen[ amount ];
+                    str += oneToNineteen[ amount ];
                 return str;
             }
             else
@@ -96,66 +98,85 @@ public class ChequeWriting{
                     str += tens[amount / highestPower/10] + oneToNineteen[(amount/highestPower)%10]+  numToWordMap.get(highestPower)  ;
                 else
                     str += oneToNineteen[ amount / highestPower] + numToWordMap.get(highestPower) ;
-                
+            
             str += numToStr(amount % highestPower);
         }
-        
-        else if (amount < 20) {
-            
-            str += oneToNineteen[ amount ];
-        }
-        
-        else {
-              str += tens[amount / 10];
-              str += numToStr(amount % 10);
-        }
-        
         return str;
     }
     
-    public static String process(String rupee){
-        boolean hasDenom = false; 
-        String lastDenom = null;
-         
-        for(String denom : denoms){
-
-            if(rupee.contains(denom)){                    
-                 if(!hasDenom){
-                    hasDenom = true;
-                    lastDenom = denom;
-                 }
-                 else{
-                     rupee=rupee.replace(lastDenom,lastDenom+" AND");
-                     break;
-                 }
-                   
+    private static String fixAnd(String rupee){
+        ArrayList<String> denomArray = new ArrayList<String>();
+        denomArray.add("BILLION");
+        denomArray.add("MILLION");
+        denomArray.add("THOUSAND");
+        denomArray.add("AND");
+        
+        String [] splittedRupee = rupee.split(" ");
+        for(int i=0;i<splittedRupee.length;i++)
+        {
+            if(splittedRupee[i].equals("HUNDRED") && !denomArray.contains(splittedRupee[i+1]))
+            {
+                splittedRupee[i] = splittedRupee[i].replace("HUNDRED", "HUNDRED AND");
             }
         }
-        for(String denom : denoms){
-            if(rupee.replace(" AND","").indexOf(denom)+denom.length()+1 >=  rupee.replace(" AND","").length()-1){
-                rupee = rupee.replace(" AND","");
-            }
+        String finalCurrency=new String("");
+        for(String s:splittedRupee)
+        {
+            finalCurrency+=s+" ";
         }
-        
-        
-        return rupee;    
+        return finalCurrency;
+    }
+    
+    public static String threeDigits(int s)
+    {
+        int quotient = s/100;
+        int reminder = s%100;
+        return numToStr(s);
         
     }
+    
+    public static String printInWesternCurrency(int n)
+    {
+        Map<Integer, String> MultipleofTens = new HashMap(){{
+            put(1000000000,"TRILLION ");
+            put(1000000, "MILLION ");
+            put(1000,"THOUSAND ");
+            put(100,"HUNDRED ");
+            put(1,"");}};
+        
+        String RupeesInWords = "";
+        
+        int[] denom = {1000000000, 1000000, 1000, 100, 1};
+        
+        int Rupees = n;
+        if(Rupees == 0)
+            return "Zero Rupees only ";
+        for(int j=0; j < denom.length; j++)
+        {
+            int d = Rupees/denom[j];
+            if(d>0)
+            {
+                RupeesInWords += numToStr(d) + MultipleofTens.get(denom[j]);
+            }
+            
+            Rupees = Rupees % denom[j];
+        }
+        return RupeesInWords ;
+    }
+    
     public static void main(String[] args){
-        String amount = "9389008.99";
-    	String paise = getPaise(amount);
-		paise = updatePaiseFormat(paise);
-		paise = paiseWords(paise);
+        String amount = "900389008.99";
+        String paise = getPaise(amount);
+        paise = updatePaiseFormat(paise);
+        paise = paiseWords(paise);
         String rupee = getRupee(amount);
-        rupee = numToStr(Integer.parseInt(rupee));
-        rupee = process(rupee) + "RUPEES ";
+        rupee = printInWesternCurrency(Integer.parseInt(rupee));
+        rupee = fixAnd(rupee) + "RUPEES ";
         if(paise.length() > 2)
             rupee += "AND " + paise.toUpperCase()+"PAISE";
-            
         rupee += " ONLY";
-        
-	System.out.println(rupee);
+        System.out.println(rupee);
         
     }
-
+    
 }
